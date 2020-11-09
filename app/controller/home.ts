6925afae111ffdfac977c6d95b77cf4cb5107179
vimniky <vimniky@gmail.com>
2020-11-09 23:29:13 +0800

    Initialize

---
+++ app/controller/home.ts
@@ -0,0 +1,9 @@
+import { Controller } from 'egg';
+
+export default class HomeController extends Controller {
+  public async index() {
+    const { ctx } = this;
+    const msg = await ctx.service.test.sayHi('egg');
+    ctx.body = { msg };
+  }
+}
---
+++ config/config.default.ts
@@ -0,0 +1,12 @@
+import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg';
+
+export default (appInfo: EggAppInfo) => {
+  const config = {} as PowerPartial<EggAppConfig>;
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1604935278509_2925';
+
+  return {
+    ...config,
+  };
+};
